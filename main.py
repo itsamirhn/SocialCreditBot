a9c7de464418f2d562f20b18ef33b1ce49fb60c0
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-03-02 12:40:13 +0330

    Remove git from forward messages

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def save_update(f):
 def forward_update(f):
     def g(update: Update, context: CallbackContext):
         response = f(update, context)
-        if (Filters.video | Filters.photo | Filters.document)(update):
+        if ((Filters.video | Filters.photo | Filters.document) & ~Filters.document.gif)(update):
             update.message.forward(BACKUP_CHANNEL_ID)
         return response
     return g
